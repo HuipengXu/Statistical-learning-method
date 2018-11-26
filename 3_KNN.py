8d433ba7199d65c079d896102700865c44ce909a
Brycexxx <2531094755@qq.com>
2018-11-26 16:32:42 +0800

    add nearest_neighbor

--- 3_KNN.py
+++ 3_KNN.py
@@ -1,36 +1,38 @@
 import numpy as np 
 
-# 二叉树节点类
 class Node:
-
-    def __init__(self, val: np.ndarray):
+    """
+    kd 树节点类
+    """
+    def __init__(self, val: np.ndarray, label):
         self.val = val
+        self.label = label
+        self.parent = None
         self.left = None
         self.right = None
 
     def __repr__(self):
         return "<Node: %s>" % str(self.val)
 
-# 构造平衡 kd 树
-class KDTree:
-
-    def __init__(self, k=1):
-        self._k = k
+class NearestNeighbor:
+    """
+    最近邻
+    """
+    def __init__(self):
         self._depth = 0
+        self._root = None
 
-    def fit(self, X: np.ndarray):
+    # 构造平衡 kd 树
+    def _generate(self, X: np.ndarray, y: np.ndarray, k: int):
         """
-        X: dataset, shape = (n_samples, n_features)
+        k: dimension of dataset
         """
-        _, features = X.shape
-        self._k = self._k if self._k <= features else features
-        return self._generate(X)
-        
-    def _generate(self, X: np.ndarray):
         samples, _ = X.shape
         if samples == 0:
             return None
-        split_dimension = self._depth % self._k
+        # 选择切分维度
+        split_dimension = self._depth % k
+        # 选择中位数
         if samples == 1:
             median = 0
         else:
@@ -40,22 +42,74 @@ class KDTree:
                 median = samples // 2
             X = X[X[:, split_dimension].argsort()]
         self._depth += 1
-        root = Node(X[median])
-        root.left = self._generate(X[:median])
-        root.right = self._generate(X[median+1:])
+        root = Node(X[median], y[median])
+        root.left = self._generate(X[:median], y[:median], k)
+        if root.left:
+            root.left.parent = root
+        root.right = self._generate(X[median+1:], y[:median], k)
+        if root.right:
+            root.right.parent = root
         return root
 
+    # 训练
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        """
+        X: dataset, shape = (n_samples, n_features)
+        """
+        _, features = X.shape
+        self._root = self._generate(X, y, features)
+        return self
+
+    # 搜索最近叶节点
+    def nearest_leaf_search(self, root: Node, x: np.ndarray, dimension: int=0):
+        if not (root.left or root.right):
+            return root
+        features = x.shape[0]
+        if x[dimension] < root.val[dimension]:
+            if not root.left:
+                return root
+            dimension = (dimension + 1) % features
+            return self.nearest_leaf_search(root.left, x, dimension)
+        elif x[dimension] == root.val[dimension]:
+            return root
+        else:
+            if not root.right:
+                return root
+            dimension = (dimension + 1) % features
+            return self.nearest_leaf_search(root.right, x, dimension)
+    
+    # 搜索最近邻
+    def nearest_neighbor_search(self, root: Node, x: np.ndarray):
+        """
+        root: KDTree 的根节点
+        x: 待搜索的目标点
+        """
+        nearest_node = self.nearest_leaf_search(root, x) 
+        current_node = nearest_node.parent
+        min_distance = np.linalg.norm(x - nearest_node.val)
+        while current_node:
+            child = current_node.left if current_node.right == nearest_node else current_node.right
+            for node in [child, current_node]:
+                distance = np.linalg.norm(x - node.val)
+                if distance < min_distance:
+                    min_distance = distance
+                    nearest_node = node
+            current_node = current_node.parent
+        return nearest_node
+    
+    def predict(self, X: np.ndarray):
+        """
+        X: dataset, shape = (n_samples, n_features)
+        """
+        ret = []
+        for x in X:
+            nearest_neighbor = self.nearest_leaf_search(self._root, x)
+            ret.append(nearest_neighbor.label)
+        return np.array(ret)
+        
+
 if __name__ == "__main__":
-    kdt = KDTree(2)
     test = np.array([[2, 3], [5, 4], [9, 6], [4, 7], [8, 1], [7, 2]])
-    root = kdt.fit(test)
-    stack = [root]
-    while stack:
-        node = stack.pop()
-        print(node.val)
-        if node.right:
-            stack.append(node.right)
-        if node.left:
-            stack.append(node.left)
+    
 
     

924ba726a614974f694a3dd676d4736350b42b21
Brycexxx <2531094755@qq.com>
2018-11-27 12:16:11 +0800

    kd 树构造错误

--- 3_KNN.py
+++ 3_KNN.py
@@ -1,5 +1,6 @@
 import numpy as np 
 
+# TODO kd 树构造错误
 class Node:
     """
     kd 树节点类
@@ -19,19 +20,19 @@ class NearestNeighbor:
     最近邻
     """
     def __init__(self):
-        self._depth = 0
         self._root = None
 
     # 构造平衡 kd 树
-    def _generate(self, X: np.ndarray, y: np.ndarray, k: int):
+    def _generate(self, X: np.ndarray, y: np.ndarray, k: int, depth: int=0):
         """
         k: dimension of dataset
         """
+        left_depth = right_depth = depth
         samples, _ = X.shape
         if samples == 0:
             return None
         # 选择切分维度
-        split_dimension = self._depth % k
+        split_dimension = depth % k
         # 选择中位数
         if samples == 1:
             median = 0
@@ -41,12 +42,14 @@ class NearestNeighbor:
             else:
                 median = samples // 2
             X = X[X[:, split_dimension].argsort()]
-        self._depth += 1
+            y = y[X[:, split_dimension].argsort()]
+        left_depth += 1
         root = Node(X[median], y[median])
-        root.left = self._generate(X[:median], y[:median], k)
+        root.left = self._generate(X[:median], y[:median], k, left_depth)
         if root.left:
             root.left.parent = root
-        root.right = self._generate(X[median+1:], y[:median], k)
+        right_depth += 1
+        root.right = self._generate(X[median+1:], y[median+1:], k, right_depth)
         if root.right:
             root.right.parent = root
         return root
@@ -65,36 +68,38 @@ class NearestNeighbor:
         if not (root.left or root.right):
             return root
         features = x.shape[0]
-        if x[dimension] < root.val[dimension]:
+        if x[dimension] <= root.val[dimension]:
+            dimension = (dimension + 1) % features
             if not root.left:
-                return root
-            dimension = (dimension + 1) % features
+                return self.nearest_leaf_search(root.right, x, dimension)
             return self.nearest_leaf_search(root.left, x, dimension)
-        elif x[dimension] == root.val[dimension]:
-            return root
         else:
-            if not root.right:
-                return root
             dimension = (dimension + 1) % features
+            if not root.right:
+                return self.nearest_leaf_search(root.left, x, dimension)
             return self.nearest_leaf_search(root.right, x, dimension)
     
     # 搜索最近邻
-    def nearest_neighbor_search(self, root: Node, x: np.ndarray):
+    def nearest_neighbor_search(self, x: np.ndarray):
         """
         root: KDTree 的根节点
         x: 待搜索的目标点
         """
-        nearest_node = self.nearest_leaf_search(root, x) 
+        nearest_node = self.nearest_leaf_search(self._root, x) 
         current_node = nearest_node.parent
         min_distance = np.linalg.norm(x - nearest_node.val)
+        child = nearest_node
         while current_node:
-            child = current_node.left if current_node.right == nearest_node else current_node.right
-            for node in [child, current_node]:
-                distance = np.linalg.norm(x - node.val)
-                if distance < min_distance:
-                    min_distance = distance
-                    nearest_node = node
+            other_child = current_node.left if current_node.right == child else current_node.right
+            for node in [other_child, current_node]:
+                if node:
+                    distance = np.linalg.norm(x - node.val)
+                    if distance < min_distance:
+                        min_distance = distance
+                        nearest_node = node
+            child = current_node
             current_node = current_node.parent
+        # print(min_distance)
         return nearest_node
     
     def predict(self, X: np.ndarray):
@@ -103,13 +108,37 @@ class NearestNeighbor:
         """
         ret = []
         for x in X:
-            nearest_neighbor = self.nearest_leaf_search(self._root, x)
+            nearest_neighbor = self.nearest_neighbor_search(x)
             ret.append(nearest_neighbor.label)
         return np.array(ret)
         
 
 if __name__ == "__main__":
     test = np.array([[2, 3], [5, 4], [9, 6], [4, 7], [8, 1], [7, 2]])
-    
+    from sklearn.datasets import load_iris
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score
+    from sklearn.neighbors import KNeighborsClassifier, KDTree
+
+    iris = load_iris()
+    nn = NearestNeighbor()
+    X = iris.data
+    y = iris.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    nn.fit(X_train, y_train)
+    y_pre = nn.predict(X_test)
+    # print(y_test)
+    print(accuracy_score(y_test, y_pre))
+    # knn = KNeighborsClassifier(n_neighbors=1, algorithm='kd_tree').fit(X_train, y_train)
+    # y_ = knn.predict(X_test)
+    # print(accuracy_score(y_test, y_))
+    # print('----------------')
+    # nn = NearestNeighbor().fit(X_train, y_train)
+    # nn = NearestNeighbor().fit(X_train, y_train)
+    # for x in X_test:
+        # print(nn.nearest_neighbor_search(x))
+
+
+
 
     

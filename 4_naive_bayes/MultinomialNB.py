32632850956fcdaf502cc1f772c751c955ee4a5a
Brycexxx <2531094755@qq.com>
2018-12-01 09:18:38 +0800

    add MultinomialNB

--- 4_naive_bayes/MultinomialNB.py
+++ 4_naive_bayes/MultinomialNB.py
@@ -1,4 +1,5 @@
 import numpy as np 
+from sklearn.preprocessing import  LabelBinarizer
 from scipy.misc import logsumexp
 from collections import Counter
 from BaseNB import BaseNaiveBayes
@@ -17,40 +18,25 @@ class MultinomialNaiveBayes(BaseNaiveBayes):
         X: train dataset, shape = (n_samples, n_features)
         y: target, shape = (n_samples, )
         """
-        # 计算 y 的先验概率
-        y_prior_proba = []
-        # features = X.shape[1]
-        self.classes_ = np.unique(y)
-        for c in self.classes_:
-            c_count = (y==c).sum()
-            y_prior_proba.append(c_count / np.size(y))
-        # y 的先验概率
-        self._y_prior_proba = np.array(y_prior_proba)
-        # # 特征 Xi 在所属类别 yk 中出现的总次数
-        # self._N_yk_xi = []
-        # 所有特征在 yk 中出现的总次数
-        self._N_yk = np.zeros((np.size(self.classes_), 1))
-        self._N_yk_xj = []
-        for i in range(np.size(self.classes_)):
-            x_given_y = X[y==self.classes_[i]]
-            self._N_yk_xj.append([Counter(x) for x in x_given_y.T])
-            self._N_yk[i, :] = x_given_y.shape[0]
+        # 0-1 化
+        label_binarizer = LabelBinarizer().fit(y) 
+        # Y.shape = (n_samples, classes_)
+        Y = label_binarizer.transform(y)
+        # 处理二分类的时候返回一列的情况
+        if Y.shape[1] == 1:
+            Y = np.concatenate((1 - Y, Y), axis=1)
+        self.classes_ = label_binarizer.classes_
+        # self._y_prior_log_proba.shape = (classes_, )
+        self._y_prior_log_proba = np.log(Y.sum(axis=0) / Y.shape[0])
+        # self._N_yk_xi.shape = (classes_, n_features)
+        self._N_yk_xi = np.dot(Y.T, X) + self._alpha
+        self._N_yk = self._N_yk_xi.sum(axis=1)
+        self._features_log_proba = np.log(self._N_yk_xi) - np.log(self._N_yk.reshape(-1, 1)) 
         return self
 
     def _joint_log_likelihood(self, X: np.ndarray):
-        rows, features = X.shape
-        classes_nums = np.size(self.classes_)
-        log_proba = np.zeros((rows, classes_nums))
-        for m in range(rows): 
-            for i in range(classes_nums):
-                x_given_y_prior = np.zeros((1, features))
-                for j in range(features):
-                    numerator = self._N_yk_xj[i][j].get(X[m, j], 0) + self._alpha
-                    denominator = self._N_yk[i] + self._alpha * features
-                    x_given_y_prior[:, j] = numerator / denominator
-                log_x_given_y_prior = np.sum(np.log(x_given_y_prior)) + np.log(self._y_prior_proba[i])
-                log_proba[m, i] = log_x_given_y_prior
-        return log_proba
+        jll = np.dot(X, self._features_log_proba.T) + self._y_prior_log_proba
+        return jll
 
     def __repr__(self):
         return "<MultinomialNaiveBayes>" 
@@ -60,15 +46,29 @@ if __name__ == "__main__":
     from sklearn.metrics import accuracy_score
     from sklearn.model_selection import train_test_split
     from sklearn.naive_bayes import MultinomialNB
+    from sklearn.feature_extraction.text import CountVectorizer
+    import pandas as pd 
 
     X = np.array([[1, 'S'], [1, 'M'], [1, 'M'], [1, 'S'], [1, 'S'], [2, 'S'], [2, 'M'], 
                     [2, 'M'], [2, 'L'], [2, 'L'], [3, 'L'], [3, 'M'], [3, 'M'], [3, 'L'], [3, 'L']])
     y = np.array([-1, -1, 1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1])
-    # iris = load_iris()
-    # X = iris.data 
-    # y = iris.target
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-    mnb = MultinomialNaiveBayes(0).fit(X, y)
-    y_pred = mnb.predict(X)
-    print(accuracy_score(y.T, y_pred))
-    # print(X[1, :])
+
+    data = open(r'4_naive_bayes\corpus').read()
+    labels, texts = [], []
+    for line in data.split('\n'):
+        content = line.split(maxsplit=1)
+        labels.append(content[0][-1])
+        texts.append(content[1])
+
+    data = pd.DataFrame(data={'labels': labels, 'texts': texts})
+    X_train, X_test, y_train, y_test = train_test_split(data['texts'], data['labels'].apply(int), random_state=0)
+    vect = CountVectorizer(ngram_range=(1, 3), min_df=3, analyzer='char_wb').fit(X_train)
+    X_train_vectorized = vect.transform(X_train)
+    X_test_vectorized = vect.transform(X_test)
+
+    mnb = MultinomialNaiveBayes().fit(X_train_vectorized.toarray(), y_train)
+    y_pred = mnb.predict(X_test_vectorized.toarray())
+    print(accuracy_score(y_test, y_pred))
+    print('-----------------------------------')
+    mNB = MultinomialNB().fit(X_train_vectorized, y_train)
+    print(mNB.score(X_test_vectorized, y_test))

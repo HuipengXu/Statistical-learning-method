0ba25081529d5c92415aff35409541c90e37ec93
Brycexxx <2531094755@qq.com>
2018-12-06 22:19:37 +0800

    后剪枝待测试

--- 5_decision_tree/decision_tree_regressor.py
+++ 5_decision_tree/decision_tree_regressor.py
@@ -1,5 +1,7 @@
 import numpy as np 
 from typing import Tuple
+from sklearn.model_selection import train_test_split
+from sklarn.metrics import mean_squared_error
 
 class Node:
 
@@ -8,8 +10,9 @@ class Node:
         self._val = val
         # 节点所包含样本的均方差
         self._mse = mse
-        # 
+        # 节点所包含样本的数量
         self._samples = samples
+        # 节点的最优分割点
         self._best_pair = pair
         self._left = None
         self._right = None
@@ -19,8 +22,9 @@ class Node:
 
 class DTRegressor:
 
-    def __init__(self):
+    def __init__(self, is_pre_pruning: bool=True):
         self.root = None
+        self._is_pre_pruning = is_pre_pruning
 
     def _split(self, x: np.ndarray, y: np.ndarray, split_feature_index: int, split_point: float):
         """
@@ -30,7 +34,7 @@ class DTRegressor:
         right_index = x[:, split_feature_index] > split_point
         return x[left_index, :], x[right_index, :], y[left_index], y[right_index]    
 
-    def _generate_regression_tree(self, X: np.ndarray, y: np.ndarray):
+    def _generate_regression_tree(self, X: np.ndarray, y: np.ndarray, is_pre_pruning: bool=True):
         """
         递归生成最小二乘回归树
         """
@@ -38,7 +42,13 @@ class DTRegressor:
         best_feature = best_point = None
         pair = (best_feature, best_point)
         y_var = y.var()
-        min_loss = y_var * np.size(y)
+        # 是否进行预剪枝
+        if self._is_pre_pruning
+            min_loss = y_var * np.size(y)  
+        else:
+            min_loss = np.inf
+            # 为后剪枝留出独立验证集
+            X, self._X_validation, y, self._y_validation = train_test_split(X, y, test_size=0.1, random_state=0)
         rows, features = X.shape
         # 如果样本数量少于 2 ，则停止分割，生成叶节点
         if rows < 2:
@@ -65,7 +75,7 @@ class DTRegressor:
                     min_loss = loss
         pair = (best_feature, best_point)
         root = Node(y.mean(), y_var, rows, pair)
-        # 如果遍历完没找到最优分割特征，则停止分割，生成叶节点
+        # 如果遍历完没找到最优分割特征，则停止分割，生成叶节点, 配合预剪枝
         if best_feature is None:
             return root
         left_x, right_x, left_y, right_y = self._split(X, y, best_feature, best_point)
@@ -84,11 +94,73 @@ class DTRegressor:
         self.root = self._generate_regression_tree(X, y)
         return self
 
-    def _poster_pruning(self):
+    def _get_pruned_tree(self, X: np.ndarray, y: np.ndarray, root: Node):
+        """"""
+        alpha = np.inf
+        # 只有根节点
+        if not any((root._left, root._right)):
+            return (alpha, root)
+        children = [root._left, root._right]
+        min_gt_tree = root
+        while children:
+            child = children.pop(0)
+            # 跳过叶节点
+            if not any((child._left, child._right)):
+                continue
+            children.append(child._left)
+            children.append(child._right)
+            # 以内部节点为单节点的预测误差
+            c_t = np.sum((y - child._val) ** 2)
+            T_t_results = []
+            # 内部节点的叶结点个数
+            leaf_nums = 0
+            child_sub_node = [child._left, child._right]
+            while child_sub_node:
+                sub_node = child_sub_node.pop(0)
+                if not any((sub_node._left, sub_node._right)):
+                    leaf_nums += 2
+                    continue
+                child_sub_node.append(sub_node._left)
+                child_sub_node.append(sub_node._right)
+            # 求以内部节点为根节点的子树的预测误差
+            for x in X:
+                split_feature, split_point = child._best_pair
+                node = child
+                while split_feature != None:
+                    if x[split_feature] <= split_point:
+                        node = node._left
+                    else:
+                        node = node._right
+                    split_feature, split_point = node._best_pair
+                T_t_results.append(node._val)
+            T_t_results = np.array(T_t_results)
+            c_T_t = np.sum((y - T_t_results) ** 2)
+            # 剪枝阈值
+            g_t = (c_t - c_T_t) / (leaf_nums - 1)
+            if g_t < alpha:
+                alpha = g_t
+                min_gt_tree = child\
+        # 将内部节点转换为叶节点
+        min_gt_tree._left = None
+        min_gt_tree._right = None
+        min_gt_tree._best_pair = (None, None)
+        return (alpha, root)
+        
+    def _poster_pruning(self, X: np.ndarray, y: np.ndarray):
         """
         对已经生成的决策树进行剪枝
         """
+        sub_trees_series = [(np.inf, self.root)]
+        pruned_tree = self.root
+        # 构造最优子树序列
+        while True:
+            alpha, pruned_tree = self._get_pruned_tree(X, y, pruned_tree)
+            sub_trees_series.append((alpha, pruned_tree))
+            if not any((pruned_tree._left._best_pair[0], pruned_tree._right._best_pair[0])):
+                break
+        # 交叉验证获得最优子树
         
+
 
     def predict(self, X: np.ndarray):
         """

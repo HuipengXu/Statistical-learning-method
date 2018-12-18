8cec2ec1782d43e42ebfc21dd264663fa966e06a
Brycexxx <2531094755@qq.com>
2018-12-18 20:36:35 +0800

    完成最大熵模型学习的 BFGS 算法（对于大数据集存在效率低下问题）

--- 6_logistic_and_maximum_entropy/maximum_entropy.py
+++ 6_logistic_and_maximum_entropy/maximum_entropy.py
@@ -73,26 +73,9 @@ class MaximumEntropyBFGS:
     def _cost(self, X:np.ndarray, w: np.ndarray):
         w = np.concatenate((w[:self.features], w[self.features:]), axis=1)
         P_x = X / X.shape[0]
-        log_zw = np.exp(np.dot(X, w)).sum(axis=1, keepdims=True)
-        cost = np.multiply(P_x, log_zw).sum() - np.multiply(self._E_P_wave, np.concatenate((w[:, 0], w[:, 1]), axis=0)).sum()
+        log_zw = np.log(np.exp(np.dot(X, w)).sum(axis=1, keepdims=True))
+        cost = np.multiply(P_x.prod(axis=1, keepdims=True), log_zw).sum() - np.multiply(self._E_P_wave, np.concatenate((w[:, 0], w[:, 1]), axis=0)).sum()
         return cost
-
-    # 确定步长 lambda_
-    # Armijo 条件: f(xk + lambda_ * pk) <= f(xk) + lambda_ * c * g * pk
-    def _backtracking_line_search(self, X:np.ndarray, g_w: np.ndarray, p: np.ndarray):
-        # 初始化 c, tau lambda_
-        c = np.random.random_sample()
-        tau = np.random.random_sample()
-        lambda_ = 10.0
-        while True:
-            new_w = self._w + lambda_ * p
-            # Armijo 条件左边
-            Armijo_l = self._cost(X, new_w)
-            # Armijo 条件左边
-            Armijo_r = self._cost(X, self._w) + lambda_ * c * np.multiply(g_w, p).sum()
-            if Armijo_l <= Armijo_r:
-                return lambda_
-            lambda_ *= tau
 
     def _calculate_gradient(self, X: np.ndarray):
         pyx = self._calculate_pyx(X)
@@ -100,7 +83,24 @@ class MaximumEntropyBFGS:
         g_w = E_P - self._E_P_wave
         return g_w
 
+    # 确定步长 lambda_
+    # Armijo 条件: f(xk + lambda_ * pk) <= f(xk) + lambda_ * c * pk.T * g
+    def _backtracking_line_search(self, X:np.ndarray, g_w: np.ndarray, p: np.ndarray, c, tau):
+        lambda_ = 10.0
+        while True:
+            new_w = self._w + lambda_ * p
+            # Armijo 条件左边
+            Armijo_l = self._cost(X, new_w)
+            # Armijo 条件右边
+            Armijo_r = self._cost(X, self._w) + lambda_ * c * np.dot(p.T, g_w)
+            if Armijo_l <= Armijo_r:
+                return lambda_
+            lambda_ *= tau
+
     def fit(self, X: np.ndarray, y: np.ndarray):
+        # 初始化 c, tau
+        c = np.random.random_sample()
+        tau = np.random.random_sample()
         n_samples, self.features = X.shape
         lb = LabelBinarizer().fit(y)
         self.classes = lb.classes_
@@ -110,22 +110,28 @@ class MaximumEntropyBFGS:
         self._E_P_wave = xy_freq / n_samples
         self._w = np.zeros_like(self._E_P_wave)
         # 初始化对称正定矩阵 B
-        B = make_spd_matrix(self._w.shape[0])
+        # B_inv = np.eye(self._w.shape[0])
+        I = np.eye(self._w.shape[0])
+        # B = make_spd_matrix(self._w.shape[0], random_state=0)
+        B_inv = I
         g_w = self._calculate_gradient(X)
         for _ in range(self._max_iter):
-            p = np.dot(np.linalg.inv(B), -g_w)
-            lambda_ = self._backtracking_line_search(X, g_w, p)
+            p = - np.dot(B_inv, g_w)
+            lambda_ = self._backtracking_line_search(X, g_w, p, c, tau)
             last_w = deepcopy(self._w)
             self._w += lambda_ * p
             last_g_w = deepcopy(g_w)
             g_w = self._calculate_gradient(X)
             if np.linalg.norm(g_w) < self._eps:
                 break
-            delta_g = g_w - last_g_w
-            delta_w = self._w - last_w
-            # TODO 直接更新为逆？
-            B = B + np.dot(delta_g, delta_g.T) / (np.dot(delta_g.T, delta_w) + 1e-9) -\
-                np.dot(np.dot(np.dot(B, delta_w), delta_w.T), B) / (np.dot(np.dot(delta_w.T, B), delta_w) + 1e-9)
+            yk= g_w - last_g_w
+            sk = self._w - last_w
+            # B = B + np.dot(yk, yk.T) / (np.dot(yk.T, s) + 1e-9) -\
+            #     np.dot(np.dot(np.dot(B, s), s.T), B) / (np.dot(np.dot(s.T, B), s) + 1e-9)
+            rho = 1 / (np.dot(yk.T, sk) + 1e-20)
+            B_l = (I - rho * np.dot(sk, yk.T))
+            B_r = (I - rho * np.dot(yk, sk.T))
+            B_inv = np.dot(B_l, np.dot(B_inv, B_r)) + rho * np.dot(sk, sk.T)
         return self
 
     def predict_proba(self, X: np.ndarray):
@@ -144,38 +150,39 @@ if __name__ == "__main__":
     from sklearn.feature_extraction.text import CountVectorizer
     from sklearn.preprocessing import binarize
 
-    with open(r'..\ttest\data.txt', 'r') as f:
-        X = []
-        y = []
-        for line in f.readlines():
-            l = line.split()
-            X.append(' '.join(l[1:]))
-            y.append(l[0])
-    y = np.array(y)
-    y = np.where(y=='yes', 1, 0)
-    vect = CountVectorizer()
-    vectorized_X = vect.fit_transform(X)
-    me = MaximumEntropyBFGS().fit(vectorized_X.toarray(), y)
-    x_test = ['sunny hot high FALSE', 'overcast hot high FALSE', 'sunny cool high TRUE']
-    x_test_vect = vect.transform(x_test)
-    pred = me.predict(x_test_vect.toarray())
-    print(pred)
+    # with open(r'..\ttest\data.txt', 'r') as f:
+    #     X = []
+    #     y = []
+    #     for line in f.readlines():
+    #         l = line.split()
+    #         X.append(' '.join(l[1:]))
+    #         y.append(l[0])
+    # y = np.array(y)
+    # y = np.where(y=='yes', 1, 0)
+    # vect = CountVectorizer()
+    # vectorized_X = vect.fit_transform(X)
+    # me = MaximumEntropyIIS().fit(vectorized_X.toarray(), y)
+    # x_test = ['sunny hot high FALSE', 'overcast hot high FALSE', 'sunny cool high TRUE']
+    # x_test_vect = vect.transform(x_test)
+    # pred = me.predict(vectorized_X.toarray())
     # print(accuracy_score(y, pred))
+    # pred = me.predict(x_test_vect.toarray())
+    # print(pred)
 
-    # data = open(r'..\4_naive_bayes\corpus').read()
-    # labels, texts = [], []
-    # for line in data.split('\n'):
-    #     content = line.split(maxsplit=1)
-    #     labels.append(content[0][-1])
-    #     texts.append(content[1])
-    #
-    # data = pd.DataFrame(data={'labels': labels, 'texts': texts})
-    # X_train, X_test, y_train, y_test = train_test_split(data['texts'], data['labels'].apply(int), random_state=0)
-    # vect = CountVectorizer(ngram_range=(1, 3), min_df=3).fit(X_train)
-    # X_train_vectorized = vect.transform(X_train).toarray()
-    # X_train_vectorized = binarize(X_train_vectorized)
-    # X_test_vectorized = vect.transform(X_test).toarray()
-    # X_test_vectorized = binarize(X_test_vectorized)
-    # me = MaximumEntropyBFGS(500).fit(X_train_vectorized, y_train)
-    # y_pred = me.predict(X_test_vectorized)
-    # print(accuracy_score(y_test, y_pred))
+    data = open(r'..\4_naive_bayes\corpus').read()
+    labels, texts = [], []
+    for line in data.split('\n'):
+        content = line.split(maxsplit=1)
+        labels.append(content[0][-1])
+        texts.append(content[1])
+
+    data = pd.DataFrame(data={'labels': labels, 'texts': texts})
+    X_train, X_test, y_train, y_test = train_test_split(data['texts'], data['labels'].apply(int), random_state=0)
+    vect = CountVectorizer(min_df=5).fit(X_train)
+    X_train_vectorized = vect.transform(X_train).toarray()
+    X_train_vectorized = binarize(X_train_vectorized)
+    X_test_vectorized = vect.transform(X_test).toarray()
+    X_test_vectorized = binarize(X_test_vectorized)
+    me = MaximumEntropyBFGS(500).fit(X_train_vectorized, y_train)
+    y_pred = me.predict(X_test_vectorized)
+    print(accuracy_score(y_test, y_pred))

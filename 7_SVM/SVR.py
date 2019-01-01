9a014ef44a3cce58aa5b43abe4f9bf996f93053c
Brycexxx <2531094755@qq.com>
2019-01-01 14:59:28 +0800

    待做。。。。

---
+++ 7_SVM/SVR.py
@@ -0,0 +1,18 @@
+# @Time    : 2018/12/31 10:21
+# @Author  : Xu Huipeng
+# @Blog    : https://brycexxx.github.io/
+
+import numpy as np
+
+class SupportVectorRegressor:
+
+    def __init__(self):
+        pass
+
+
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        pass
+
+
+    def predict(selfself, X: np.ndarray, y: np.ndarray):
+        pass

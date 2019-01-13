15e80642dce40166b59835a802af370edc10a81f
Brycexxx <2531094755@qq.com>
2019-01-13 20:58:28 +0800

    添加 viterbi 预测算法

--- HMM/hidden_Markov_model.py
+++ HMM/hidden_Markov_model.py
@@ -71,6 +71,7 @@ class HiddenMarkovModel:
         n_samples, length = X.shape
         obs_set = np.unique(X)
         n_obs = np.size(obs_set)
+        # 随机初始化参数,保证每行相加等于 1
         rs = np.random.RandomState(self.random_state)
         transmat_prob = rs.random_sample((self.n_states, self.n_states))
         self.transmat_prob = transmat_prob / transmat_prob.sum(axis=1, keepdims=True)
@@ -90,10 +91,10 @@ class HiddenMarkovModel:
                 for t in range(length - 1):
                     xi_numerator = alpha[t].reshape(-1, 1) * self.transmat_prob * \
                                    self.obs_prob[:, x[t + 1]] * beta[t + 1]
-                    xi_t += xi_numerator / (xi_numerator.sum() + 1e-500)
+                    xi_t += xi_numerator / (xi_numerator.sum())
                 xi += xi_t
                 gamma_numerator = alpha * beta
-                gamma_i = gamma_numerator / (gamma_numerator.sum(axis=1, keepdims=True) + 1e-500)
+                gamma_i = gamma_numerator / (gamma_numerator.sum(axis=1, keepdims=True))
                 gamma += gamma_i
                 for k in range(n_obs):
                     idx = x == obs_set[k]
@@ -102,13 +103,31 @@ class HiddenMarkovModel:
             old_transmat = self.transmat_prob.copy()
             old_obs = self.obs_prob.copy()
             self.start_prob = gamma[0] / n_samples
-            self.transmat_prob = xi / (gamma[:length - 1].sum(axis=0, keepdims=True).T + 1e-500)
-            self.obs_prob = obs_numerator / (gamma.sum(axis=0).reshape(-1, 1) + 1e-500)
+            self.transmat_prob = xi / (gamma[:length - 1].sum(axis=0, keepdims=True).T)
+            self.obs_prob = obs_numerator / (gamma.sum(axis=0).reshape(-1, 1))
             if np.linalg.norm(self.start_prob - old_start) < self.eps and \
                     np.linalg.norm(self.transmat_prob - old_transmat) < self.eps and \
                     np.linalg.norm(self.obs_prob - old_obs) < self.eps: return self
         warn("Maybe hmm doesn't converge!")
         return self
+
+    # 使用 viterbi 算法预测
+    def predict(self, X: np.ndarray):
+        n_samples, length = X.shape
+        state_seq = np.zeros((n_samples, length), dtype=np.int16)
+        for i in range(n_samples):
+            delta = np.zeros((length, self.n_states))
+            delta[0] = self.start_prob * self.obs_prob[:, X[i, 0]]
+            psi = np.zeros((length, self.n_states))
+            for t in range(1, length):
+                temp_prob = delta[t - 1].reshape(-1, 1) * self.transmat_prob
+                delta[t] = temp_prob.max(axis=0) * self.obs_prob[:, X[i, t]]
+                psi[t] = temp_prob.argmax(axis=0)
+            state_seq[i, -1] = delta[-1].argmax()
+            # 最优路径回溯
+            for t in range(length - 2, -1, -1):
+                state_seq[i, t] = psi[t, state_seq[i, t + 1]]
+        return state_seq
 
 
 if __name__ == "__main__":
@@ -117,6 +136,7 @@ if __name__ == "__main__":
     # hmm.transmat_prob = np.array([[0.5, 0.2, 0.3], [0.3, 0.5, 0.2], [0.2, 0.3, 0.5]])
     # hmm.obs_prob = np.array([[0.5, 0.5], [0.4, 0.6], [0.7, 0.3]])
     from hmmlearn.hmm import MultinomialHMM
+    from sklearn.metrics import accuracy_score
     import numpy as np
 
     np.set_printoptions(precision=5, suppress=True)
@@ -130,19 +150,22 @@ if __name__ == "__main__":
     hmm_generate.start_prob = np.array([0.2, 0.4, 0.4])
     hmm_generate.transmat_prob = np.array([[0.5, 0.2, 0.3], [0.3, 0.5, 0.2], [0.2, 0.3, 0.5]])
     hmm_generate.obs_prob = np.array([[0.5, 0.5], [0.4, 0.6], [0.7, 0.3]])
-    X, _ = hmm_generate.simulate(1000)
-    hmm = HiddenMarkovModel(n_states=3, eps=0.01, max_iter=500)
-    hmm.fit(X.reshape(1, -1))
-    print('初始状态分布概率：')
-    print(hmm.start_prob)
-    print('状态转移概率矩阵：')
-    print(hmm.transmat_prob)
-    print(hmm.transmat_prob.sum(axis=1, keepdims=True))
-    print('观测概率矩阵：')
-    print(hmm.obs_prob)
-    print(hmm.obs_prob.sum(axis=1))
+    X, I = hmm_generate.simulate(100)
+    # hmm = HiddenMarkovModel(n_states=3, eps=0.01, max_iter=500)
+    # hmm.fit(X.reshape(1, -1))
+    # print('初始状态分布概率：')
+    # print(hmm.start_prob)
+    # print('状态转移概率矩阵：')
+    # print(hmm.transmat_prob)
+    # print(hmm.transmat_prob.sum(axis=1, keepdims=True))
+    # print('观测概率矩阵：')
+    # print(hmm.obs_prob)
+    # print(hmm.obs_prob.sum(axis=1))
+    # I_pred = hmm.predict(X.reshape(1, -1))
+    # print(accuracy_score(I, I_pred.squeeze()))
     hmmL = MultinomialHMM(n_components=3).fit(X.reshape(-1, 1))
     print('from hmmlearn:')
     print(hmmL.startprob_)
     print(hmmL.transmat_)
     print(hmmL.emissionprob_)
+    print(accuracy_score(I, hmmL.predict(X.reshape(-1, 1))))
